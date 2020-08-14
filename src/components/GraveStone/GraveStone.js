f341725b92e9fbd438476aa40daa8c32d976f988
Matt Logan <mlogan5212@gmail.com>
2020-08-13 20:33:45 -0500

    completed GraveYard and GraveStone components

---
+++ src/components/GraveStone/GraveStone.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import studentShape from '../../helpers/data/props/studentShape';
+
+class GraveStone extends React.Component {
+  static propTypes = {
+    gravestone: studentShape.studentShape,
+  }
+
+  render() {
+    const { gravestone } = this.props;
+
+    return (
+      <div className="deadman">
+      <i className="fas fa-dizzy fa-2x"></i>
+      <p>{gravestone.firstName}</p>
+      </div>
+    );
+  }
+}
+
+export default GraveStone;
